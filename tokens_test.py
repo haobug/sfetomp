5c8f9745bf5e75002ab824556eee3db3d8c523c9
qinghao <qingxianhao@gmail.com>
2016-02-15 21:37:25 +0800

    one more test point 2

--- tokens_test.py
+++ tokens_test.py
@@ -7,16 +7,20 @@ class AccessTokensTest(unittest.TestCase):
     def test_get_access_token(self):
         tokenfile = "token6.json"
         obj = tokens.AccessToken()
-        obj.get_access_token(
+        token = obj.get_access_token(
             appid="",  # fill
             secret="",  # fill
             filename=tokenfile
         )
         obj2 = tokens.AccessToken()
-        obj2.token_load("token.json")
+        token2 = obj2.token_load("token.json")
         print obj2
-        self.assertNotEqual(obj, obj2)
+        self.assertNotEqual(token, token2)
 
+        obj3 = tokens.AccessToken()
+        token3 = obj3.token_load("token6.json")
+        print obj3
+        self.assertEqual(token, token3)
 
 if __name__ == '__main__':
     unittest.main()
